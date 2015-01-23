4f4124c34dd2397b07eb4ead76d5a942d71d1c0b
stephjin <stephanie.k.jin@gmail.com>
2015-01-23 14:11:46 -0800

    Write server using pure node.

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
 // NODE SERVER VIA PURE NODE
+var http = require('http'),
+    fs = require('fs');
+
+    http.createServer(function(req, res){
+      res.writeHead(200, {
+        'Content-Type': 'text/html',
+        'Access-Control-Allow-Origin': '*'
+      });
+
+      var readStream = fs.createReadStream(__dirname + '/index.html');
+
+      readStream.pipe(res);
+    }).listen(3000);
+
+    console.log('Visit me at http://localhost:3000');
 
 // NODE SERVER VIA EXPRESS
