e6fdc69e87bc22710a0a8092ce10127c225f4c83
stephjin <stephanie.k.jin@gmail.com>
2015-01-23 18:47:28 -0800

    Create single user delete.

--- server.js
+++ server.js
@@ -100,7 +100,14 @@ apiRouter.route('/users/:user_id')
     });
   })
 
-  ;
+  .delete(function(req,res){
+    User.remove({
+      _id: req.params.user_id
+    }, function(err,user){
+      if (err) res.send(err);
+      res.json({ message: 'Successfully deleted' });
+    });
+  });
 
 app.use('/api', apiRouter);
 
