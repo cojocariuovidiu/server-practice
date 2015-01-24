133fb0dc8260190c463a846466ec4669159fa739
stephjin <stephanie.k.jin@gmail.com>
2015-01-23 18:00:44 -0800

    Create users get.

--- server.js
+++ server.js
@@ -70,6 +70,13 @@ apiRouter.route('/users')
       }
       res.json({ message: 'User created!' });
     });
+  })
+
+  .get(function(req,res){
+    User.find(function(err,users){
+      if (err) res.send(err);
+      res.json(users);
+    });
   });
 
 app.use('/api', apiRouter);
