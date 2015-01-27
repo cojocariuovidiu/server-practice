db4699d9f664cd8b62cb64738eefd3b0fa94f719
stephjin <stephanie.k.jin@gmail.com>
2015-01-26 17:28:04 -0800

    Complete functions for authentication factory.

--- public/app/services/auth-service.js
+++ public/app/services/auth-service.js
@@ -5,13 +5,53 @@ angular.module('authService', [])
 .factory('Auth', function($http, $q, AuthToken){
   var authFactory = {};
 
+  // make request to node api, if true will return successful callback
+  authFactory.login = function(username, password){
+    return $http.post('/api/authenticate', {
+      username: username.
+      password: password
+    })
+    .success(function(data){
+      AuthToken.setToken(data.token);
+    });
+  };
+
+  authFactory.logout = function(){
+    AuthToken.setToken();
+  };
+
+  authFactory.isLoggedIn = function(){
+    if (AuthToken.getToken())
+      return true;
+    else
+      return false;
+  };
+
+  authFactory.getUser = function(){
+    if (AuthToken.getToken())
+      return $http.get('/api/me');
+    else
+      return $q.reject({ message: 'User has no token.' });
+  };
+
   return authFactory;
 })
 
 // token auth functions; handles getting and saving the token
-// $window stores token client-side
+// $window stores token client-side. can find local storage via resources in dev tools
 .factory('AuthToken', function($window){
   var authTokenFactory = {};
+
+  authTokenFactory.getToken = function(){
+    return $window.localStorage.getItem('token');
+  };
+
+  authTokenFactory.setToken = function(token){
+    if (token)
+      $window.localStorage.setItem('token', token);
+    else
+      $window.localStorage.removeItem('token');
+  };
 
   return authTokenFactory;
 })
@@ -19,6 +59,19 @@ angular.module('authService', [])
 // auth interceptor; handles attaching token to HTTP requests, redirects if not logged in
 .factory('AuthInterception', function($q, AuthToken){
   var interceptorFactory = {};
+
+  interceptorFactory.request = function(config){
+    var token = AuthToken.getToken();
+    if (token)
+      config.headers['x-access-token'] = token;
+    return config;
+  };
+
+  interceptorFactory.responseError = function(response){
+    if (response.status == 403)
+      $location.path('/login');
+    return $q.reject(response);
+  };
 
   return interceptorFactory;
 });
