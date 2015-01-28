e5463f05fd2efbc92edddcb66540d1e8ac8b63c3
stephjin <stephanie.k.jin@gmail.com>
2015-01-28 12:03:49 -0800

    Create delete user controller.

--- public/app/controllers/userCtrl.js
+++ public/app/controllers/userCtrl.js
@@ -2,21 +2,26 @@ angular.module('userCtrl', ['userService'])
 
 .controller('userController', function(User){
   var vm = this;
+    
+  vm.processing = true;
+
+  User.all()
+    .success(function(data){
+      vm.processing = false;
+      vm.users = data;
+    });
 
   vm.deleteUser = function(id){
-    
     vm.processing = true;
-    
     User.delete(id)
       .success(function(data){
+        vm.processing = false;
         User.all()
           .success(function(data){
-            vm.processing = false;
             vm.users = data;
           });
       });
   };
-
 })
 
 .controller('userCreateController', function(User){
